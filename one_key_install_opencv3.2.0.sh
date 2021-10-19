2e87523bc502ee6cb8267f58916082d119ac73e8
Jowisdom <276380251@qq.com>
2021-10-19 22:34:07 +0800

    modify

--- one_key_install_opencv3.2.0.sh
+++ one_key_install_opencv3.2.0.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-echo "Installing opencv3.2.0......"
+echo "Installing opencv3.4.16......"
 sudo apt-get install build-essential
 sudo apt-get install libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
 sudo apt update
 sudo apt install libjasper1 libjasper-dev
 sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-wget -O opencv-3.2.0.zip https://codeload.github.com/opencv/opencv/zip/3.2.0
-unar opencv-3.2.0.zip
-mkdir build-opencv
-cd build-opencv
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local -DENABLE_PRECOMPILED_HEADERS=OFF ../opencv-3.2.0
+wget -O opencv-3.4.16.zip https://codeload.github.com/opencv/opencv/zip/3.4.16
+unar opencv-3.4.16.zip
+cd opencv-3.4.16
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make -j8
 sudo make install
-cd ..
-echo "opencv3.2.0 has been installed successfully"
+cd
+
