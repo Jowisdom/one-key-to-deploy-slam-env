e42b822d9a6c822879af5b2274669958d1688488
jowisdom <276380251@qq.com>
2021-11-09 21:45:35 +0800

    add nutstore

--- one_key_install_ubuntu_app.sh
+++ one_key_install_ubuntu_app.sh
@@ -3,6 +3,8 @@
 set -x # echo on
 set -e # exit on error
 
+sudo apt install cmake 
+
 # install typora
 # or run:
 # sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
@@ -16,15 +18,27 @@ sudo apt-get install typora
 #install joplin
 wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash 
 
-#install clion,pycharm,vscode,fireware,zotero
-sudo snap install zotero-snap 
-sudo snap install flameshot 
-sudo snap install clion --classic 
-sudo snap install pycharm-professional --classic 
-
 # install unar
 sudo apt install unar 
 # install stacer
 sudo add-apt-repository ppa:oguzhaninan/stacer 
 sudo apt-get update 
 sudo apt-get install stacer  
+
+# install nutstore
+sudo apt-get install libglib2.0-dev libgtk2.0-dev libnautilus-extension-dev gvfs-bin python3-gi gir1.2-appindicator3-0.1
+cd ~/Downloads
+wget https://www.jianguoyun.com/static/exe/installer/nutstore_linux_src_installer.tar.gz
+tar zxf nutstore_linux_src_installer.tar.gz
+cd nutstore_linux_src_installer && ./configure && make
+sudo make install
+nautilus -q
+./runtime_bootstrap
+
+
+#install clion,pycharm,vscode,fireware,zotero
+sudo snap install zotero-snap 
+sudo snap install flameshot 
+sudo snap install clion --classic 
+sudo snap install pycharm-professional --classic 
+
